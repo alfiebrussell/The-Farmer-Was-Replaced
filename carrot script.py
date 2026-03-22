be1920c0b50b9b5aa443b554e83a0c12c6eb458c
Alfie <55883979+alfiebrussell@users.noreply.github.com>
2026-03-22 21:36:50 +0000

    inital Commiit

---
+++ carrot script.py
@@ -0,0 +1,32 @@
+while True:
+	if can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		#get_water()
+		#use_item(Items.Water)
+	else:
+		plant(Entities.Carrot)
+		#get_water()
+		#use_item(Items.Water)
+		
+
+	x = get_pos_x()
+	y = get_pos_y()
+		
+	# If X is even (0, 2, 4...)
+	if x % 2 == 0:
+		if y < 5:
+			move(North)
+		else:
+			move(East)
+	# If X is odd (1, 3, 5...)
+	else:
+		if y > 0:
+			move(South)
+		else:
+			# If we are at the last column (x=5) and at the bottom (y=0)
+			if x == 5: 
+				while get_pos_x() > 0:
+					move(West)
+			else:
+				move(East)
---
+++ till script.py
@@ -0,0 +1,24 @@
+while True:
+	if get_ground_type() == Grounds.Soil:
+		till()
+
+	x = get_pos_x()
+	y = get_pos_y()
+
+	# If X is even (0, 2, 4...)
+	if x % 2 == 0:
+		if y < 5:
+			move(North)
+		else:
+			move(East)
+	# If X is odd (1, 3, 5...)
+	else:
+		if y > 0:
+			move(South)
+		else:
+			# If we are at the last column (x=5) and at the bottom (y=0)
+			if x == 5: 
+				while get_pos_x() > 0:
+					move(West)
+			else:
+				move(East)
